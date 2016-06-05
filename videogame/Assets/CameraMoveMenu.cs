6616e07198c402c42671e80df0c9c855216e9ada
Leonel Badi <lbadi@itba.edu.ar>
2016-06-05 16:12:17 -0300

    Menu

---
+++ videogame/Assets/CameraMoveMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoveMenu : MonoBehaviour {
+
+	int rotCamera = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.rotation.y > 30) {
+			rotCamera = -1;
+		}
+		if (this.transform.rotation.y > 30) {
+			rotCamera = 1;
+		}
+		this.transform.Rotate (new Vector3 (0, rotCamera * Time.deltaTime, 0));
+	}
+}
---
+++ videogame/Assets/_CompletedAssets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadScene(string name){
+		StartCoroutine (loadLevel ());
+	}
+
+	public void quitGame(){
+		Application.Quit();
+	}
+
+	public IEnumerator loadLevel(){
+		yield return new WaitForSeconds (1.0f);
+		SceneManager.LoadScene(1);
+	}
+}
